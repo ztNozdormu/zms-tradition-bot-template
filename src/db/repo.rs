1400a216f1ba0288e44ea20f80988c2201896363
www1 <w1999wtw3537@sina.com>
2025-03-24 10:11:22 +0800

    sqlite connection init

--- src/db/repo.rs
+++ src/db/repo.rs
@@ -1,22 +1,15 @@
-use diesel::sqlite::SqliteConnection;
-use bb8::Pool;
-use bb8_diesel::DieselConnectionManager;
-use std::{env, sync::Mutex};
-use once_cell::sync::Lazy;
+use std::env;
 
-type SqlitePool = Pool<DieselConnectionManager<SqliteConnection>>;
+use diesel::{r2d2, SqliteConnection};
 
-pub static GLOBAL_CONNECTION: Lazy<Mutex<SqlitePool>> = Lazy::new(|| Mutex::new(establish_connection()));
+pub fn new_connection_pool() -> r2d2::Pool<r2d2::ConnectionManager<SqliteConnection>> {
 
-pub fn establish_connection() -> SqlitePool {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    println!("database_url{:?}", database_url);
-    // Create the Diesel connection manager
-    let manager = DieselConnectionManager::<SqliteConnection>::new(database_url);
-    // Create the connection pool with a max size of 10
-    Pool::builder()
-        .max_size(10)
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    let manager = r2d2::ConnectionManager::<SqliteConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .max_size(15)
         .build(manager)
-        .expect("Failed to create pool")
+        .expect("Failed to create pool.");
+    pool
 }
-
